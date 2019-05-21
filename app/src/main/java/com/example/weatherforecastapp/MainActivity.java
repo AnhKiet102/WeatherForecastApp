f7029718aeb917727154f06d118933fee4a96eab
Phạm Anh Kiệt <anhkietpham.hcmus@gmail.com>
2019-05-21 23:24:19 +0700

    commit change read api

--- app/src/main/java/com/example/weatherforecastapp/MainActivity.java
+++ app/src/main/java/com/example/weatherforecastapp/MainActivity.java
@@ -1,11 +1,20 @@
 package com.example.weatherforecastapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,6 +28,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         AnhXa();
+        btnSeach.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String city= editTextSearch.getText().toString();
+                GetcurrentWeatherData(city);
+            }
+        });
+    }
+    public  void GetcurrentWeatherData(String data)
+    {
+        RequestQueue requestQueue= Volley.newRequestQueue(MainActivity.this);
+        String url= "https://api.openweathermap.org/data/2.5/weather?q="+data+"&units=metric&appid=edaee9bfa304c001a6eb1da0cc4e90ce";
+        StringRequest stringRequest= new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.d("ketqua",response);
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        requestQueue.add(stringRequest);
     }
 
     private void AnhXa() {
